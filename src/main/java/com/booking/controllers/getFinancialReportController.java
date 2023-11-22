8b8f12feb8d0d68bca4db90b31bc6198b0feb53d
teodord25 <djuric.teodor25@gmail.com>
2023-11-22 17:45:15 +0100

    third set of controllers

---
+++ src/main/java/com/booking/controllers/getFinancialReportController.java
@@ -0,0 +1,24 @@
+package com.booking.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/api/getNotifications")
+public class getFinancialReportController {
+    // TODO: implement
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Collection<Boolean>> getFinancialReport() {
+        // FinancialReport financialReport = someService.getFinancialReport();
+
+        // return new ResponseEntity<Collection<Boolean>>(financialReport, HttpStatus.OK);
+
+        return new ResponseEntity<Collection<Boolean>>(HttpStatus.OK);
+    }
+}
