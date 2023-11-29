755f3febe4e7adc5acd1e6fc36f4a01fec8e7a84
teodord25 <djuric.teodor25@gmail.com>
2023-11-29 16:30:27 +0100

    fix minor mistake

--- src/main/java/com/booking/controller/ReviewController.java
+++ src/main/java/com/booking/controller/ReviewController.java
@@ -6,12 +6,7 @@ import com.booking.dto.RetrieveOwnerReviewDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -72,14 +67,11 @@ public class ReviewController {
         return new ResponseEntity<>(reviews, HttpStatus.OK);
     }
 
-    @GetMapping (
-            produces = MediaType.APPLICATION_JSON_VALUE,
+    @DeleteMapping(
             path = "/{reviewId}"
     )
     public ResponseEntity<List<CreateOwnerReviewDTO>> deleteReview(@PathVariable Long reviewId) {
-        List<CreateOwnerReviewDTO> reviews = new ArrayList<>(); // implement service
-
-        return new ResponseEntity<>(reviews, HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
 
