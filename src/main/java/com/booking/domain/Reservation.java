542dc06f9312ac4d747d9c2fbf8f82f6abc83888
teodord25 <djuric.teodor25@gmail.com>
2023-12-18 15:49:27 +0100

    refactor model

--- src/main/java/com/booking/domain/Reservation.java
+++ src/main/java/com/booking/domain/Reservation.java
@@ -3,7 +3,10 @@ package com.booking.domain;
 import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDate;
-
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.Date;
+import java.util.List;
 import java.util.Date;
 import java.util.List;
 
