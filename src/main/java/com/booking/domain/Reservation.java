06c77edb35d5d7d6868ebe69f73a504f07601270
teodord25 <djuric.teodor25@gmail.com>
2023-12-18 15:49:27 +0100

    refactor model

--- src/main/java/com/booking/domain/Reservation.java
+++ src/main/java/com/booking/domain/Reservation.java
@@ -1,13 +1,38 @@
 package com.booking.domain;
 
-import java.util.Date;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.util.Date;
+import java.util.List;
+
+@ToString
+@EqualsAndHashCode
+@Entity
+@Table(name = "accommodation")
+@NoArgsConstructor
+@Getter
+@Setter
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "startDate")
     private Date startDate;
+
+    @Column(name = "endDate")
     private Date endDate;
+
+    @Column(name = "guestCount")
     private int guestCount;
+
+    @Column(name = "reservationStatus")
     private ReservationStatus status;
+
+    @Column(name = "totalPrice")
     private int totalPrice;
 
     public Reservation(Long id, Date startDate, Date endDate, int guestCount,
@@ -18,67 +43,5 @@ public class Reservation {
         this.guestCount = guestCount;
         this.status = status;
         this.totalPrice = totalPrice;
-    }
-
-    // Getters and setters for each field
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
-    }
-
-    public Date getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
-    }
-
-    public int getGuestCount() {
-        return guestCount;
-    }
-
-    public void setGuestCount(int guestCount) {
-        this.guestCount = guestCount;
-    }
-
-    public ReservationStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(ReservationStatus status) {
-        this.status = status;
-    }
-
-    public int getTotalPrice() {
-        return totalPrice;
-    }
-
-    public void setTotalPrice(int totalPrice) {
-        this.totalPrice = totalPrice;
-    }
-
-    // ToString method to represent reservation details as a String
-    @Override
-    public String toString() {
-        return "Reservation{" +
-                "id=" + id +
-                ", startDate=" + startDate +
-                ", endDate=" + endDate +
-                ", guestCount=" + guestCount +
-                ", status=" + status +
-                ", totalPrice=" + totalPrice +
-                '}';
     }
 }
