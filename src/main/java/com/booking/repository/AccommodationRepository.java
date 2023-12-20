e3307defadbd75a3accc151af7d4d7bdf6717cf6
teodord25 <djuric.teodor25@gmail.com>
2023-12-20 11:37:03 +0100

    remove stupid lazy loading method

--- src/main/java/com/booking/repository/AccommodationRepository.java
+++ src/main/java/com/booking/repository/AccommodationRepository.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 @Repository
 public interface AccommodationRepository extends GenericRepository<Accommodation> {
-    public Accommodation getById(Long id);
 
     public List<Accommodation> findAll();
 
