77e855029bf68545cbbb54ccf116759aa607e6e5
Teodor Đurić <70525484+teodord25@users.noreply.github.com>
2023-12-29 11:48:36 +0100

    empty controllers (#8) (#26)
    * final models (#3)
    
    * controllers (#5)
    
    * controllers (#6)
    
    * third set of controllers (#7)
    
    ---------
    
    Co-authored-by: Dragoslav Tamindžija <dragodev@protonmail.com>
    Co-authored-by: petar76-2021 <105112524+petar76-2021@users.noreply.github.com>

---
+++ src/main/java/com/booking/controllers/confirmEmailController.java
@@ -0,0 +1,23 @@
+package com.booking.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/confirmEmail")
+public class confirmEmailController {
+    // TODO: implement
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Boolean> confirmEmail(@RequestBody Boolean data) throws Exception {
+        // Boolean success = userService.confirmEmail(data);
+        // if (success) {
+           return new ResponseEntity<>(HttpStatus.CREATED);
+        // }
+        // return a 4XX status
+    }
+}
