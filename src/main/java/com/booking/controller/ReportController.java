02cbb9fa99396c8c924fe0aff197e5ce3b1a3477
Teodor Đurić <70525484+teodord25@users.noreply.github.com>
2024-06-05 17:14:53 +0200

    Feature/block and report user (#36)
    * impl block user
    
    * impl user report

--- src/main/java/com/booking/controller/ReportController.java
+++ src/main/java/com/booking/controller/ReportController.java
@@ -2,6 +2,8 @@ package com.booking.controller;
 
 import com.booking.dto.Report.ReviewReportDTO;
 import com.booking.dto.Report.UserReportDTO;
+import com.booking.service.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,14 +12,21 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/report")
 public class ReportController {
+    private final ReportService reportService;
+
+    @Autowired
+    public ReportController(ReportService reportService) {
+        this.reportService = reportService;
+    }
+
     @PostMapping(
-            path = "/userReport",
+            path = "/user",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     public ResponseEntity<UserReportDTO> createUserReport(@RequestBody UserReportDTO userReportDTO) {
-        if (userReportDTO.equals(null)) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        return new ResponseEntity<>(new UserReportDTO(), HttpStatus.OK);
+        this.reportService.createUserReport(userReportDTO);
+        return new ResponseEntity<>(userReportDTO, HttpStatus.OK);
     }
 
     @PutMapping(
